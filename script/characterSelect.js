c7d18f6ec3187991968be4f5d1bfeed2582fc129
Reisli Hysa <reislihysa@Isabellas-iMac.local>
2021-10-11 11:35:04 +0100

    Character selected choice leeds into the battle menu

--- script/characterSelect.js
+++ script/characterSelect.js
@@ -2,47 +2,76 @@ const chosenCharacter = document.querySelector('.chosenCharacter')
 const chosenCharacterText = document.querySelector('.chosenCharacter p')
 const grid = document.querySelectorAll('.grid div')
 const arrow = document.querySelectorAll('aside')
+const battleScreen = document.querySelector('.battle')
 
 
 function dogHover(){
     arrow[0].classList.toggle('opacity')
-    chosenCharacter.classList.toggle('character-option-dog')
+    if (chosenCharacter.classList.contains('character-option-cat')){
+        chosenCharacter.classList.remove('character-option-cat')
+    } else if (chosenCharacter.classList.contains('character-option-turtle')){
+        chosenCharacter.classList.remove('character-option-turtle')
+
+    }else if (chosenCharacter.classList.contains('character-option-snake')){
+        chosenCharacter.classList.remove('character-option-snake')
+
+    }
+    chosenCharacter.classList.add('character-option-dog')
     chosenCharacterText.textContent = 'Dog'
 }
 function dogHoverNo(){
     arrow[0].classList.toggle('opacity')
-    chosenCharacter.classList.toggle('character-option-dog')
-    chosenCharacterText.textContent = ''
 }
 function catHover(){
     arrow[1].classList.toggle('opacity')
-    chosenCharacter.classList.toggle('character-option-cat')
+    if (chosenCharacter.classList.contains('character-option-dog')){
+        chosenCharacter.classList.remove('character-option-dog')
+    } else if (chosenCharacter.classList.contains('character-option-turtle')){
+        chosenCharacter.classList.remove('character-option-turtle')
+
+    }else if (chosenCharacter.classList.contains('character-option-snake')){
+        chosenCharacter.classList.remove('character-option-snake')
+
+    }
+    chosenCharacter.classList.add('character-option-cat')
     chosenCharacterText.textContent = 'Cat'
 }
 function catHoverNo(){
     arrow[1].classList.toggle('opacity')
-    chosenCharacter.classList.toggle('character-option-cat')
-    chosenCharacterText.textContent = ''
 }
 function turtleHover(){
     arrow[3].classList.toggle('opacity')
-    chosenCharacter.classList.toggle('character-option-turtle')
+    if (chosenCharacter.classList.contains('character-option-dog')){
+        chosenCharacter.classList.remove('character-option-dog')
+    } else if (chosenCharacter.classList.contains('character-option-cat')){
+        chosenCharacter.classList.remove('character-option-cat')
+
+    }else if (chosenCharacter.classList.contains('character-option-snake')){
+        chosenCharacter.classList.remove('character-option-snake')
+
+    }
+    chosenCharacter.classList.add('character-option-turtle')
     chosenCharacterText.textContent = 'Turtle'
 }
 function turtleHoverNo(){
     arrow[3].classList.toggle('opacity')
-    chosenCharacter.classList.toggle('character-option-turtle')
-    chosenCharacterText.textContent = ''
 }
 function snakeHover(){
     arrow[2].classList.toggle('opacity')
-    chosenCharacter.classList.toggle('character-option-snake')
+    if (chosenCharacter.classList.contains('character-option-dog')){
+        chosenCharacter.classList.remove('character-option-dog')
+    } else if (chosenCharacter.classList.contains('character-option-turtle')){
+        chosenCharacter.classList.remove('character-option-turtle')
+
+    }else if (chosenCharacter.classList.contains('character-option-cat')){
+        chosenCharacter.classList.remove('character-option-cat')
+
+    }
+    chosenCharacter.classList.add('character-option-snake')
    chosenCharacterText.textContent = 'Snake'
 }
 function snakeHoverNo(){
     arrow[2].classList.toggle('opacity')
-    chosenCharacter.classList.toggle('character-option-snake')
-   chosenCharacterText.textContent = ''
 }
 
 grid[0].addEventListener('mouseover', dogHover)
@@ -63,10 +92,71 @@ class Pet{
     }
 }
 
+let characterPicked = null
+let cpuPick = null
+
 const dog = new Pet(1.5, 1.5, 100, 1.5)
 const cat = new Pet(1.5, 1.2, 100, 2)
 const snake = new Pet(2, 1.2, 100, 1.5)
 const turtle = new Pet(1.2, 2, 100, 1)
+
+function playGame(){
+let i = Math.floor(Math.random() * grid.length)
+console.log(i)
+if(i === 0){
+    cpuPick = dog
+} else if(i === 1){
+    cpuPick = cat
+} else if(i === 2){
+    cpuPick = snake
+} else if(i == 3){
+    cpuPick = turtle
+}
+characterSelectionScreen.classList.add('hidden')
+battleScreen.classList.remove('hidden')
+
+
+
+
+
+
+
+
+}
+
+function dogSelected(){
+    characterPicked = dog
+    console.log(characterPicked)
+    chosenCharacterText.textContent = 'You chose Dog!'
+    playGame()
+}
+function catSelected(){
+    characterPicked = cat
+    console.log(characterPicked)
+    chosenCharacterText.textContent = 'You chose Cat!'
+    playGame()
+}
+function snakeSelected(){
+    characterPicked = snake
+    console.log(characterPicked)
+    chosenCharacterText.textContent = 'You chose Snake!'
+    playGame()
+}
+function turtleSelected(){
+    characterPicked = turtle
+    console.log(characterPicked)
+    chosenCharacterText.textContent = 'You chose Turtle!'
+    playGame()
+}
+
+
+
+
+
+grid[0].addEventListener('click', dogSelected)
+grid[1].addEventListener('click', catSelected)
+grid[2].addEventListener('click', snakeSelected)
+grid[3].addEventListener('click', turtleSelected)
 
 // if(cat.speed > dog.speed){
 
